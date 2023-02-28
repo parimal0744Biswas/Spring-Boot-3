06fcb61ae4e2c2acdb39586b1e9fd4efb6e8b32a
PARIMAL BISWAS <biswasparimal01@gmail.com>
2023-02-28 22:44:47 +0530

    .

--- Spring/Project-1/src/main/java/com/parimal/AppGamingBasicJava.java
+++ Spring/Project-1/src/main/java/com/parimal/AppGamingBasicJava.java
@@ -5,6 +5,7 @@ public class AppGamingBasicJava
 
 	public static void main(String[] args)
 	{
+		var SuperContractGame = new SuperContractGame();
 		var mariogame = new MarioGame();
 		var gameRunner = new GameRunner(mariogame);
 		gameRunner.run();
